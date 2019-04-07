2743cfa89bf90fe32a1f5d5e4cd3819b7b0e1b77
olaftarlowski <olaf.tarlowski@gmail.com>
2019-04-07 21:44:13 +0200

    results connected with front-end

--- main.js
+++ main.js
@@ -17,38 +17,79 @@ const btnSubmit = document.querySelector(".resultSection .playButton");
 function handSelection() {
     gameChoice.playerChoice = this.dataset.option;
     hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.transition = "0.3s"
     this.style.boxShadow = "0 0 40px 10px red";
+
 }
 
 function aiHandSelection() {
     return hands[Math.floor(Math.random() * hands.length)].dataset.option;
 }
-function startGame() {
-    if(gameChoice.playerChoice == ''){
-        alert("Choose your hand");
-        return;
-    }
-    let aiResult = gameChoice.aiChoice = aiHandSelection();
-    let playerResult = gameChoice.playerChoice;
 
-    if(playerResult == aiResult) {
+function checkResult(playerResult, aiResult) {
+    
+    if (playerResult == aiResult) {
+        console.log('1');
         return 'draw';
     } else if (
-        (playerResult == 'lizard' && aiResult == 'spock') || (playerResult == 'lizard' && aiResult == 'paper') || 
+        (playerResult == 'lizard' && aiResult == 'spock') || (playerResult == 'lizard' && aiResult == 'paper') ||
         (playerResult == 'rock' && aiResult == 'scissors') || (playerResult == 'rock' && aiResult == 'lizard') ||
         (playerResult == 'paper' && aiResult == 'spock') || (playerResult == 'paper' && aiResult == 'rock') ||
         (playerResult == 'scissors' && aiResult == 'lizard') || (playerResult == 'scissors' && aiResult == 'paper') ||
-        (playerResult == 'spock' && aiResult == 'scissors') || (playerResult == 'spock' && aiResult == 'rock') 
-        ) {
+        (playerResult == 'spock' && aiResult == 'scissors') || (playerResult == 'spock' && aiResult == 'rock')
+    ) {
+        console.log('2');
         return 'win';
-    }  else {
+    } else {
+        console.log('3');
         return 'lost';
     }
-    console.log("AI: " + aiResult);
-    console.log("player:" + playerResult);
+
+}
+function endGame() {
+    document.querySelector(`[data-option="${gameChoice.playerChoice}"]`).style.boxShadow = '';
+    
+    gameChoice.playerChoice = '';
+    gameChoice.aiChoice = '';
 }
 
+function printResult(player, ai, result){
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+    document.querySelector('p.numbers span').textContent = ++gameSummary.gamesNumber;
+    let showResultField = document.querySelector('span.showResult');
+    const actualResult = document.querySelector('.printActual');
+    if (result === 'win'){
+        showResultField.textContent = "You won!";
+        showResultField.style.color = "green";
+        document.querySelector('p.wins span').textContent = ++gameSummary.winsNumber;
+   
+    actualResult.textContent = 'Player';
+
+    } else if (result === 'lost') {
+        showResultField.textContent = "You lost";
+        showResultField.style.color = "red";
+        document.querySelector('p.losses span').textContent = ++gameSummary.lossesNumber;
+        actualResult.textContent = 'AI';
+    } else {
+        showResultField.textContent = "It's a draw";
+        showResultField.style.color = "gray";
+        document.querySelector('p.draws span').textContent = ++gameSummary.drawsNumber;
+        actualResult.textContent = 'Draw';
+    }
+}
+
+function startGame(){
+    if (gameChoice.playerChoice == '') {
+        alert("Choose your hand");
+        return;
+    }
+    gameChoice.aiChoice = aiHandSelection();
+    const gameResult = checkResult(gameChoice.playerChoice, gameChoice.aiChoice);
+    console.log('wynik:' + gameResult);
+    printResult(gameChoice.playerChoice, gameChoice.aiChoice, gameResult);
+    endGame()
+}
 
 hands.forEach(hand => hand.addEventListener('click', handSelection));
 btnSubmit.addEventListener('click', startGame);
-
